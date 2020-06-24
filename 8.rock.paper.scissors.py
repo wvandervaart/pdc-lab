51cb9e4178a1c3e53cdb9b8d5104f068fe423e96
Wouter van der Vaart <wouter@terrean.net>
2020-06-24 09:32:05 +0200

    Assignment 8

---
+++ 8.rock.paper.scissors.py
@@ -0,0 +1,31 @@
+import random
+
+print('''Please pick one:
+            rock
+            scissors
+            paper''')
+
+while True:
+    game_dict = {'rock': 1, 'scissors': 2, 'paper': 3}
+    player_a = str(input("Player a: "))
+    b = random.randint(1,3)
+    a = game_dict.get(player_a)
+    dif = a - b
+
+    if dif in [-1, 2]:
+        print('player a wins.')
+        if str(input('Do you want to play another game, yes or no?\n')) == 'yes':
+            continue
+        else:
+            print('game over.')
+            break
+    elif dif in [-2, 1]:
+        print('player b wins.')
+        if str(input('Do you want to play another game, yes or no?\n')) == 'yes':
+            continue
+        else:
+            print('game over.')
+            break
+    else:
+        print('Draw.Please continue.')
+        print('')
